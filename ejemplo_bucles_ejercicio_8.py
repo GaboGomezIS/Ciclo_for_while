dbe939263d7ce632db1434d9db9761c713f005e9
GaboGomezIS <126176494+GaboGomezIS@users.noreply.github.com>
2023-05-18 09:38:56 -0500

    Create ejemplo_bucles_ejercicio_8.py

---
+++ ejemplo_bucles_ejercicio_8.py
@@ -0,0 +1,8 @@
+#ejercicios de ejemplos de bucles
+#programador :Gabriel Gomez
+#vérsion 1.0 
+n = int(input("Introduce la altura del triángulo (entero positivo): "))
+for i in range(1, n+1, 2):
+    for j in range(i, 0, -2):
+        print(j, end=" ")
+    print("")
