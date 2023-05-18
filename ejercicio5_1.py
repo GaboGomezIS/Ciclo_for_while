6cf474050d2d2ae99bf0d908af883b8fa36ce67e
GaboGomezIS <126176494+GaboGomezIS@users.noreply.github.com>
2023-05-18 09:42:45 -0500

    Create ejercicio5_1.py

---
+++ ejercicio5_1.py
@@ -0,0 +1,11 @@
+#ejercicio 5_2_1
+#  es un ejemplo de un bucle cero veces
+#programador : Gabriel Gomez
+#vérsion 1.0 
+#15 /04/2023
+n = 5
+s = 0
+while (n <= 4):
+    x = input
+    s = s + x
+print ("no ejecutable")
