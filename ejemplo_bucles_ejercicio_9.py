43deed8cb7944dc18eb492ab2a0428582adac7d3
GaboGomezIS <126176494+GaboGomezIS@users.noreply.github.com>
2023-05-18 09:39:35 -0500

    Create ejemplo_bucles_ejercicio_9.py

---
+++ ejemplo_bucles_ejercicio_9.py
@@ -0,0 +1,8 @@
+#ejercicios de ejemplos de bucles
+#programador : Gabriel Gomez
+#vérsion 1.0 
+key = "contraseña"
+password =""
+while password != key:
+    password = input("Introduce la contraseña: ")
+print("Contraseña correcta")
