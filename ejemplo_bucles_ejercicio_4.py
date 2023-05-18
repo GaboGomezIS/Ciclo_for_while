3f5c218fa092b1a102682fa3e2192dec22de320f
GaboGomezIS <126176494+GaboGomezIS@users.noreply.github.com>
2023-05-18 09:36:32 -0500

    Create ejemplo_bucles_ejercicio_4.py

---
+++ ejemplo_bucles_ejercicio_4.py
@@ -0,0 +1,6 @@
+#ejercicios de ejemplos de bucles
+#programador : Gabriel Gomez
+#vérsion 1.0 
+n = int(input("Introduce un número entero positivo: "))
+for i in range(n, -1, -1):
+    print(i, end=", ")
