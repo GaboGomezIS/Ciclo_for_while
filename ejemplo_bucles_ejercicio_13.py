0cc2e71f08bad8549f02ce169bb75a5aec145b76
GaboGomezIS <126176494+GaboGomezIS@users.noreply.github.com>
2023-05-18 09:42:08 -0500

    Create ejemplo_bucles_ejercicio_13.py

---
+++ ejemplo_bucles_ejercicio_13.py
@@ -0,0 +1,8 @@
+#ejercicios de ejemplos de bucles
+#programador : Gabriel Gomez
+#vérsion 1.0 
+while True:
+    frase = input("Introduce algo: ")
+    if frase == "salir":
+        break
+    print(frase)
